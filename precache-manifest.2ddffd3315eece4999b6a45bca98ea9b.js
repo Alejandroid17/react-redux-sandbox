9aef13d35d633502ed8233703c7baaf5b71ce3b8
Alejandro <alejandrofernandezm94@gmail.com>
2019-12-28 12:06:08 +0100

    Updates

---
+++ precache-manifest.2ddffd3315eece4999b6a45bca98ea9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f685cc3b8127e789f0147cdd39f86ac",
+    "url": "/react-redux-sandbox/index.html"
+  },
+  {
+    "revision": "d381571ada02abc5e833",
+    "url": "/react-redux-sandbox/static/css/main.3046f9cf.chunk.css"
+  },
+  {
+    "revision": "7886b7205e8326b63b4d",
+    "url": "/react-redux-sandbox/static/js/2.6f1ba0f1.chunk.js"
+  },
+  {
+    "revision": "d381571ada02abc5e833",
+    "url": "/react-redux-sandbox/static/js/main.580cdf2d.chunk.js"
+  },
+  {
+    "revision": "5700b4d3292d19a5871b",
+    "url": "/react-redux-sandbox/static/js/runtime-main.11477216.js"
+  }
+]);
